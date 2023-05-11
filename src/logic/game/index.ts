59e94527bc5a4fc2aa1d0f339ace7739ab51ce5f
sijuz <sijuz@yandex.ru>
2023-05-11 17:41:12 +0400

    s

--- src/logic/game/index.ts
+++ src/logic/game/index.ts
@@ -6,8 +6,58 @@ interface ParamConstruct {
 
 export interface ObjPixel {
     number: number,
-    type: 'default' | 'portal-blue' | 'portal-red' | 'win'
+    type: 'default' | 'portal-blue' | 'portal-red' | 'win',
+    position: number[],
+    portalInfo?: Portal
 }
+
+export interface Portal {
+    id: number,
+    type: 'blue' | 'red',
+    rotate?: number,
+    distance?: number,
+    portals:
+    {
+        number: number,
+        position: number[]
+    }[]
+
+}
+
+const portals: Portal[] = [
+    {
+        id: 1,
+        type: 'blue',
+        portals: [
+            { number: 6, position: [] },
+            { number: 45, position: [] }
+        ]
+    },
+    {
+        id: 2,
+        type: 'blue',
+        portals: [
+            { number: 12, position: [] },
+            { number: 43, position: [] }
+        ]
+    },
+    {
+        id: 3,
+        type: 'red',
+        portals: [
+            { number: 7, position: [] },
+            { number: 29, position: [] }
+        ]
+    },
+    {
+        id: 4,
+        type: 'red',
+        portals: [
+            { number: 35, position: [] },
+            { number: 81, position: [] }
+        ]
+    }
+]
 
 class Game {
     private _address: string
@@ -19,27 +69,43 @@ class Game {
         this._addressUser = params.addressUser
     }
 
-    public genArrBoard (size: number): ObjPixel[][] {
+    public genArrBoard (size: number): [ObjPixel[][], Portal[]] {
         const fullArr: ObjPixel[][] = []
 
         const Max = size * size
 
-        const portal_blue: any[][] = [ [ 5, 17 ], [ 9, 43 ] ]
+        const sizePixel = 44
 
-        const portal_red: any[][] = [ [ 7, 21 ], [ 54, 81 ], [ 34, 73 ] ]
+        const fixPixel = 1
+
+        const localPortals = portals
 
         for (let i = 0; i < size; i++) {
             let arrX: ObjPixel[] = []
             for (let i2 = 0; i2 < size; i2++) {
                 const num =  Max - i2 - (i * 10)
 
-                let type = portal_blue.findIndex(p => p[0] === num || p[1] === num) >= 0 ? 'portal-blue' : 'default'
-                if (type === 'default') type = portal_red.findIndex(p => p[0] === num || p[1] === num) >= 0 ? 'portal-red' : 'default'
-                type = num === 100 ? 'win' : type
+                const typePortal = portals.filter(
+                    p => p.portals[0].number === num || p.portals[1].number === num
+                )
+
+                const type = typePortal.length > 0 ? 'portal-' + typePortal[0].type : 'default'
+
+                if (type !== 'default') {
+                    const indexPortal = portals.findIndex(
+                        p => p.portals[0].number === num || p.portals[1].number === num
+                    )
+
+                    const numOfPortal = typePortal[0].portals[0].number === num ? 0 : 1
+
+                    console.log((i % 2 ? 10 - i2 : i2 + 1) - 1, (10 - i) - 1, localPortals[indexPortal])
+                    localPortals[indexPortal].portals[numOfPortal].position = [ (i % 2 ? 10 - i2 : i2 + 1) - 1, (10 - i) - 1 ] // x y
+                }
 
                 arrX.push({
                     number: Max - i2 - (i * 10),
-                    type: type as ObjPixel['type']
+                    type: type as ObjPixel['type'],
+                    position: [ i, i2 ]
                 })
             }
 
@@ -50,7 +116,38 @@ class Game {
             fullArr.push(arrX)
         }
 
-        return fullArr
+        for (let i = 0; i < localPortals.length; i++) {
+            if (localPortals[i].portals[0].position.length > 0) {
+                const aX = localPortals[i].portals[0].position[0] * sizePixel - (sizePixel / 2)
+                const aY = localPortals[i].portals[0].position[1] * sizePixel + (sizePixel / 2)
+
+                const bX = localPortals[i].portals[1].position[0] * sizePixel - (sizePixel / 2) + fixPixel
+                const bY = localPortals[i].portals[1].position[1] * sizePixel - (sizePixel / 2) + fixPixel
+
+                const abY = aY - bY
+                const abX = aX - bX
+                const an =  Math.atan2(abY, abX)
+
+                localPortals[i].rotate = -((an * 180) / Math.PI)
+
+                // localPortals[i].distance = Math.sqrt(((bX - aX) ** 2) + ((bY - aY) ** 2))
+
+                localPortals[i].distance = Math.sqrt((aX - bX) ** 2 + (aY - bY) ** 2)
+
+                // fullArr.filter((f) => {
+                //     const ff = f.filter(f2 => f2.number === localPortals[i].portals[0].number
+                //     || f2.number === localPortals[i].portals[1].number)
+
+                //     if (ff) {
+                //         return
+                //     }
+                // })
+            }
+        }
+
+        console.log('localPortals', localPortals)
+
+        return [ fullArr, localPortals ]
     }
 }
 
--- src/pages/board/board.tsx
+++ src/pages/board/board.tsx
@@ -6,7 +6,7 @@ import { Panel, Div, Button, Link } from '../../components'
 
 import chery from '../../img/chery.svg'
 import { addStr } from '../../logic/utils'
-import { Game, ObjPixel } from '../../logic/game'
+import { Game, ObjPixel, Portal } from '../../logic/game'
 
 interface MainProps {
     id: string,
@@ -19,6 +19,8 @@ export const BoardBlock: React.FC<MainProps> = (props: MainProps) => {
     const [ firstRender, setFirstRender ] = React.useState<boolean>(false)
 
     const [ board, setBoard ] = React.useState<ObjPixel[][] | undefined>(undefined)
+
+    const [ portals, setPortals ] = React.useState<Portal[] | undefined>(undefined)
 
     const { address } = useParams()
     const history = useNavigate()
@@ -33,7 +35,11 @@ export const BoardBlock: React.FC<MainProps> = (props: MainProps) => {
         if (address) {
             const board1 = new Game({ address: '', addressUser: '' })
 
-            setBoard(board1.genArrBoard(10))
+            const localBoard = board1.genArrBoard(10)
+
+            setBoard(localBoard[0])
+
+            setPortals(localBoard[1])
         } else {
             history('/boards')
         }
@@ -41,12 +47,39 @@ export const BoardBlock: React.FC<MainProps> = (props: MainProps) => {
 
     return (
         <div className="board">
-            {board
+            {board && portals
                 ? board.map((x, key) => (
                     <div key={key} className="line">
                         {x.map((y, key2) => (
-                            <div key={key2} className={'pixel ' + y.type}>
+                            <div key={key2} className={
+                                'pixel ' + y.type
+                            }>
                                 {y.type === 'win' ? null : y.number}
+                                {
+                                    portals.filter(p => p.portals[1].number === y.number).length > 0
+                                        ? portals.filter(p => p.portals[1].number === y.number).map((portal, key3) => (
+                                            <div
+                                                key={key3}
+                                                className="line-2"
+                                                style={{
+                                                    width: ((portal.distance ?? 0) * 1) + 'px',
+                                                    transform: 'rotate(' + portal.rotate + 'deg)'
+                                                }}
+                                            ></div>
+                                        ))
+
+                                        : portals.filter(p => p.portals[0].number === y.number).map((portal, key3) => (
+                                            <div
+                                                key={key3}
+                                                className="line-3"
+                                                style={{
+                                                    width: ((portal.distance ?? 0) * 1) + 'px',
+                                                    transform: 'rotate(' + ((portal.rotate ?? 0) + 180) + 'deg)'
+                                                }}
+                                            ></div>
+                                        ))
+
+                                }
                             </div>
                         ))}
                     </div>
