eff9bfd4f2445e577defd1acf2871c94af51ce6b
sijuz <sijuz@yandex.ru>
2023-07-13 18:11:57 +0300

    s

--- src/logic/wallet/hook.tsx
+++ src/logic/wallet/hook.tsx
@@ -53,47 +53,47 @@ export function EverWalletProvider ({ children, ever }: EverConf): JSX.Element  
     useEffect(() => {
         const initPipeline = async () => {
             // Check is we have provider
-            const hasProvider2 = await hasEverscaleProvider()
-            if (!hasProvider2) {
-                setIsInitializing(false)
-                return
-            }
+        //     const hasProvider2 = await hasEverscaleProvider()
+        //     if (!hasProvider2) {
+        //         setIsInitializing(false)
+        //         return
+        //     }
 
-            await ever.ensureInitialized()
+        //     await ever.ensureInitialized()
 
-            // Subscribe for account connected
-            const permissionsSubscriber = await ever.subscribe('permissionsChanged')
-            permissionsSubscriber.on('data', (event) => {
-                setAccount(event.permissions.accountInteraction)
-                if (event.permissions.accountInteraction) {
-                    ever.getBalance(event.permissions.accountInteraction.address).then((balanceLocal) => {
-                        setBalance(balanceLocal)
-                    })
-                }
-            })
+        //     // Subscribe for account connected
+        //     const permissionsSubscriber = await ever.subscribe('permissionsChanged')
+        //     permissionsSubscriber.on('data', (event) => {
+        //         setAccount(event.permissions.accountInteraction)
+        //         if (event.permissions.accountInteraction) {
+        //             ever.getBalance(event.permissions.accountInteraction.address).then((balanceLocal) => {
+        //                 setBalance(balanceLocal)
+        //             })
+        //         }
+        //     })
 
-            // Subscribe for network change
-            const networkSubscriber = await ever.subscribe('networkChanged')
-            networkSubscriber.on('data', (event) => {
-                setSelectedNetworkId(event.networkId)
-            })
+        //     // Subscribe for network change
+        //     const networkSubscriber = await ever.subscribe('networkChanged')
+        //     networkSubscriber.on('data', (event) => {
+        //         setSelectedNetworkId(event.networkId)
+        //     })
 
-            // Get current state
-            const currentProviderState = await ever.getProviderState()
-            // Current networkId
-            setSelectedNetworkId(currentProviderState.networkId)
-            setSelectedNetworkType(currentProviderState.selectedConnection)
-            // Current account, can be undefined.
-            setAccount(currentProviderState.permissions.accountInteraction)
+        //     // Get current state
+        //     const currentProviderState = await ever.getProviderState()
+        //     // Current networkId
+        //     setSelectedNetworkId(currentProviderState.networkId)
+        //     setSelectedNetworkType(currentProviderState.selectedConnection)
+        //     // Current account, can be undefined.
+        //     setAccount(currentProviderState.permissions.accountInteraction)
 
-            if (currentProviderState.permissions.accountInteraction) {
-                const balanceLocal = await ever.getBalance(currentProviderState.permissions.accountInteraction.address)
-                setBalance(balanceLocal)
-            }
-            // Yes we have provider
-            setHasProvider(true)
-            // Initialized;
-            setIsInitializing(false)
+        //     if (currentProviderState.permissions.accountInteraction) {
+        //         const balanceLocal = await ever.getBalance(currentProviderState.permissions.accountInteraction.address)
+        //         setBalance(balanceLocal)
+        //     }
+        //     // Yes we have provider
+        //     setHasProvider(true)
+        //     // Initialized;
+        //     setIsInitializing(false)
         }
 
         initPipeline().catch((err) => {
